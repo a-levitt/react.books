3d36336f1f09b669c3bca2d22817a3708a5068db
a-levitt <artemis.levitt@gmail.com>
2024-08-21 16:36:45 +0200

    Add several books and pass their parameters

--- src/index.js
+++ src/index.js
@@ -6,31 +6,58 @@ import './index.css'
 const BookList = () => {
     return (
         <section className='booklist'>
-            <Book />
-            <Book />
-            <Book />
-            <Book />
+            <Book   
+                title={firstBook.title} 
+                author={firstBook.author} 
+                image={firstBook.image} 
+                imageAlt={firstBook.imageAlt} 
+            />
+            <Book   
+                title={secondBook.title} 
+                author={secondBook.author} 
+                image={secondBook.image} 
+                imageAlt={secondBook.imageAlt} 
+            />
+            <Book   
+                title={thirdBook.title} 
+                author={thirdBook.author} 
+                image={thirdBook.image} 
+                imageAlt={thirdBook.imageAlt} 
+            />
         </section>
     );
 };
 
-const Book = () => {
+const Book = ({title, author, image, imageAlt}) => {
     return (
     <article className='book'>
-        <Image />
-        <Title />
-        <Author />
+        <img src={image} alt={imageAlt}/>
+        <h2>{title}</h2>
+        <h4>{author}</h4>
     </article>
     );
 };
 
-const Image = () => <img src='https://f.media-amazon.com/images/I/7156r9Yg5OL._SL1360_.jpg'
-                         alt='Android Programming for Beginners'/>;
-const Title = () => <h2>Android Programming for Beginners (Third edition)</h2>;
-const Author = () => {
-    return <h4>John Horton</h4>;
+const firstBook = {
+    title: "Android Programming for Beginners (Third edition)",
+    author: "John Horton",
+    image: "./images/Android_programming.jpg",
+    imageAlt: "Android Programming for Beginners"
 };
 
+const secondBook = {
+    title: "Learning React: Modern Patterns for Developing React Apps (2nd Edition)",
+    author: "Alex Banks, Eve Porcello",
+    image: "./images/Learning_react.jpg",
+    imageAlt: "Learning React: Modern Patterns for Developing React Apps"
+};
+
+const thirdBook = {
+    title: "Head First Kotlin: A Brain-Friendly Guide (1st Edition)",
+    author: "Dawn Griffiths, David Griffiths",
+    image: "./images/HeadFirst_Kotlin.jpg",
+    imageAlt: "Head First Kotlin: A Brain-Friendly Guide"
+};
 
 
 
