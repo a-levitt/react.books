e05e493cdb65f7c1be1709122f80b6ae27e75377
a-levitt <artemis.levitt@gmail.com>
2024-08-21 13:47:03 +0200

    Implement base stucture

--- src/index.js
+++ src/index.js
@@ -2,16 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-const RootFunction = () => {
+const BookList = () => {
     return (
-
+        <section>
+            <Book />
+            <Book />
+            <Book />
+        </section>
     );
 };
-//export default RootFunction;
+
+const Book = () => {
+    return (
+    <article>
+        <Image />
+        <Title />
+        <Author />
+    </article>
+    );
+};
+
+const Image = () => <h2>image placeholder</h2>;
+const Title = () => {
+    return <h2>Book Title</h2>;
+};
+const Author = () => <h4>Autor</h4>;
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<RootFunction />);
+root.render(<BookList />);
 
  
