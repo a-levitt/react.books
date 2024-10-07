a587a2cee44c15313a479e17f5e815db748c9c0b
Artem Trifonov <artemis.levitt@gmail.com>
2024-10-07 11:49:00 +0200

    Display indexes for books

--- src/Book.js
+++ src/Book.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 const Book = (props) => {
-    const {title, author, image, imageAlt} = props;
+    const {title, author, image, imageAlt, number} = props;
     // console.log(props);
     return (
         <article className='book'>
             <img src={image} alt={imageAlt}/>
             <h2>{title}</h2>
             <h4>{author}</h4>
+            <span className='number'>{`# ${number + 1}`}</span>
         </article>
     );
 };
