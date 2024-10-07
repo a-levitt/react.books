c5063593a548430132c5b3206867b0d15b72cb35
Artem Trifonov <artemis.levitt@gmail.com>
2024-10-07 11:11:38 +0200

    Use local images

--- src/books.js
+++ src/books.js
@@ -1,22 +1,26 @@
+import img1 from "./images/Android_programming.jpg"
+import img2 from "./images/Learning_react.jpg"
+import img3 from "./images/HeadFirst_Kotlin.jpg"
+
 export const books= [
     {
         title: "Android Programming for Beginners (Third edition)",
         author: "John Horton",
-        image: "./images/Android_programming.jpg",
+        image: img1,
         imageAlt: "Android Programming for Beginners",
         id: 1
     },
     {
         title: "Learning React: Modern Patterns for Developing React Apps (2nd Edition)",
         author: "Alex Banks, Eve Porcello",
-        image: "./images/Learning_react.jpg",
+        image: img2,
         imageAlt: "Learning React: Modern Patterns for Developing React Apps",
         id: 2
     },
     {
         title: "Head First Kotlin: A Brain-Friendly Guide (1st Edition)",
         author: "Dawn Griffiths, David Griffiths",
-        image: "./images/HeadFirst_Kotlin.jpg",
+        image: img3,
         imageAlt: "Head First Kotlin: A Brain-Friendly Guide",
         id: 3
     }
